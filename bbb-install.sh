8d6137ae9665bfe2a7485973ff4f9d0b4707cd70
hoangbinh <hoangbinh@yahoo.com>
2019-03-24 19:10:16 +0700

    Update bbb-install.sh

--- bbb-install.sh
+++ bbb-install.sh
@@ -397,6 +397,7 @@ check_version() {
 check_host() {
   need_pkg dnsutils
   DIG_IP=$(dig +short $1 | grep '^[.0-9]*$' | tail -n1)
+  DIG_IP="192.168.1.9"
   if [ -z "$DIG_IP" ]; then err "Unable to resolve $1 to an IP address using DNS lookup."; fi
   get_IP
   if [ "$DIG_IP" != "$IP" ]; then err "DNS lookup for $1 resolved to $DIG_IP but didn't match local $IP."; fi
